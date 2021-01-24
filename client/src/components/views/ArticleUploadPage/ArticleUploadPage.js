8fdf7cab32c9a572e132511f3cc3f47e37787fdd
jiseong-choi <jiseong1951@kakao.com>
2021-01-24 12:14:17 +0900

    saved

---
+++ client/src/components/views/ArticleUploadPage/ArticleUploadPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ArticleUploadPage() {
+    return (
+        <div>
+            ArticleUploadPage
+        </div>
+    )
+}
+
+export default ArticleUploadPage
